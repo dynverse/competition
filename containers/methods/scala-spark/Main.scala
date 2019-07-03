ee540b8fd5f96d0eebe962e742b5eaa2d2cbb5aa
Robrecht Cannoodt <rcannood@gmail.com>
2019-07-03 17:43:17 +0200

    clean up scala code by using helper functions

--- containers/methods/scala-spark/Main.scala
+++ containers/methods/scala-spark/Main.scala
@@ -13,50 +13,31 @@ import java.io.File
 import java.io.PrintWriter
 
 object Main {
+  val spark = SparkSession.
+    builder().
+    master("local[1]").
+    appName("Comp1").
+    getOrCreate()
+
+  import spark.implicits._
+
   def main(args: Array[String]) {
     val datasetLocation = args(0)
     val outputFolder = args(1)
-    
-    // start a spark session
-    val spark = SparkSession.
-      builder().
-      master("local[4]").
-      appName("PCAExample").
-      getOrCreate()
-    
-    // import sql context
-    import spark.implicits._
-    
-    // define helper function for writing a data frame to a file
-    def dataFrameToCsv(df: DataFrame, file: String) = {
-      val header = df.columns.mkString(",") + "\n"
-      val body = df.map(_.mkString(",")).select("value").rdd.map(r => r(0)).collect().mkString("\n")
-      val text = header + body + "\n"
-      val writer = new PrintWriter(file)
-      writer.write(text)
-      writer.close()
-    }
-    
+
     /* ##### Read data ##### */
-    // reading the sparse matrix from h5
-    val reader = HDF5Factory.openForReading(datasetLocation)
+    val (cell_ids, mat) = readDataset(datasetLocation)
 
-    val dims = reader.readIntArray("data/expression/dims")
-    val i = reader.readIntArray("data/expression/i")
-    val p = reader.readIntArray("data/expression/p")
-    val x = reader.readFloatArray("data/expression/x")
-    val cell_ids = reader.readStringArray("data/expression/rownames")
-
-    val mat = new SparseMatrix(dims(0), dims(1), p, i, x.map(_.toDouble)).toSparseRowMajor
-
-    // converting sparse matrix to an array of sparse rows
-    val data = mat.colPtrs.sliding(2, 1).map(a => 
+    // convert sparse matrix to an array of sparse rows
+    val data = mat.colPtrs.sliding(2, 1).map(a =>
       Tuple1(new SparseVector(
-        mat.numCols, 
-        mat.rowIndices.slice(a(0), a(1)), 
+        mat.numCols,
+        mat.rowIndices.slice(a(0), a(1)),
         mat.values.slice(a(0), a(1))
       ))
     ).toArray
+
+    // convert to a dataset
     val df = spark.createDataFrame(data).toDF("features")
 
     /* ##### Infer a trajectory ##### */
@@ -81,7 +62,7 @@ object Main {
       (cell_ids zip pseudotime).
         map(a => (a._1, "begin", "end", a._2)).
         toSeq.
-        toDF("cell_id", "from", "to", "time")
+        toDF("cell_id", "from", "to", "percentage")
 
     /* ##### Save output ##### */
     dataFrameToCsv(milestoneNetwork, s"$outputFolder/milestone_network.csv")
@@ -89,5 +70,29 @@ object Main {
 
     // stop spark
     spark.stop()
+  }
+
+  // read the expression data from a h5 file
+  def readDataset(filename: String) = {
+    val reader = HDF5Factory.openForReading(filename)
+
+    val dims = reader.readIntArray("data/expression/dims")
+    val i = reader.readIntArray("data/expression/i")
+    val p = reader.readIntArray("data/expression/p")
+    val x = reader.readFloatArray("data/expression/x")
+    val cell_ids = reader.readStringArray("data/expression/rownames")
+
+    val matrix = new SparseMatrix(dims(0), dims(1), p, i, x.map(_.toDouble)).toSparseRowMajor
+    (cell_ids, matrix)
+  }
+
+  // write a data frame to csv
+  def dataFrameToCsv(df: DataFrame, file: String) {
+    val header = df.columns.mkString(",") + "\n"
+    val body = df.map(_.mkString(",")).select("value").rdd.map(r => r(0)).collect().mkString("\n")
+    val text = header + body + "\n"
+    val writer = new PrintWriter(file)
+    writer.write(text)
+    writer.close()
   }
 }
