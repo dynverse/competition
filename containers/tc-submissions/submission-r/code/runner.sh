5f64c580ffc754cd123062924d8f00a4ec52155e
Wouter Saelens <wouter.saelens@gmail.com>
2019-07-11 19:51:38 +0200

    update submissions

--- containers/tc-submissions/submission-r/code/runner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-input=$1
-output=$2
-entrypoint=$3
-for filename in $input/*.h5; do
-  outdir=$output/$(basename -s .h5 $filename)/
-  mkdir $outdir
-  /usr/bin/time -o $outdir/time.txt -f "%e" /code/main.R $filename $outdir
-done
--- scripts/build.sh
+++ scripts/build.sh
@@ -8,28 +8,28 @@ docker build -t $TAG $FOLDER
 docker push $TAG
 
 ## R EXAMPLE
-FOLDER="containers/methods/r/"
+FOLDER="containers/tc-submissions/submission-r/code"
 TAG="dynverse/r_example"
 
 chmod +x $FOLDER/main.R
 docker build -t $TAG $FOLDER
 
 ## PYTHON EXAMPLE
-FOLDER="containers/methods/python/"
+FOLDER="containers/tc-submissions/submission-python/code"
 TAG="dynverse/python_example"
 
 chmod +x $FOLDER/main.py
 docker build -t $TAG $FOLDER
 
 ## JULIA EXAMPLE
-FOLDER="containers/methods/julia/"
+FOLDER="containers/tc-submissions/submission-julia/code"
 TAG="dynverse/julia_example"
 
 chmod +x $FOLDER/main.jl
 docker build -t $TAG $FOLDER
 
 ## SCALA EXAMPLE
-FOLDER="containers/methods/scala-spark/"
+FOLDER="containers/tc-submissions/submission-scala-spark/code"
 TAG="dynverse/scalaspark_example"
 
 docker build -t $TAG $FOLDER
@@ -62,3 +62,11 @@ chmod +x $FOLDER/aggregate-scores.R
 docker build -t $TAG:$VERSION $FOLDER
 
 docker push $TAG:$VERSION
+
+
+## SUBMISSIONS
+zip -r containers/tc-submissions/submission-r.zip containers/tc-submissions/submission-r/
+zip -r containers/tc-submissions/submission-python.zip containers/tc-submissions/submission-python/
+zip -r containers/tc-submissions/submission-scala-spark.zip containers/tc-submissions/submission-scala-spark/
+zip -r containers/tc-submissions/submission-julia.zip containers/tc-submissions/submission-julia/
+
